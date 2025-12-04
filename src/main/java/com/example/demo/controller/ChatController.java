8dcb8c0ae0ea6c6dafd90e20ff225b04b93cc7f4
Ruvindu <ruvindusharada22@gmail.com>
2025-12-04 14:33:25 +0530

    create the ChatController.java

--- src/main/java/com/example/demo/controller/ChatController.java
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.ChatMassage;
+import lombok.Getter;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
@@ -16,6 +18,12 @@ public class ChatController {
     public ChatMassage sendMassage(ChatMassage chatMassage){
 
         return chatMassage;
+    }
+
+
+    @GetMapping
+    public String getChatMassage(){
+        return "chatMassage";
     }
 
 
