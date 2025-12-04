ec1976d7535e246fd88f94ad06b475b7e33616de
Ruvindu <ruvindusharada22@gmail.com>
2025-12-04 14:23:52 +0530

    Add the WebShorketConfig.java  over ride the methodes for the configMassageBroker and registerStrompEnpoints

--- src/main/java/com/example/demo/config/WebShorketConfig.java
+++ src/main/java/com/example/demo/config/WebShorketConfig.java
@@ -1,7 +1,9 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 
@@ -9,8 +11,17 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 
 public class WebShorketConfig implements WebSocketMessageBrokerConfigurer {
-    
 
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");// topic chat Room 1
+        registry.setApplicationDestinationPrefixes("/app");
 
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+       registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:5178").withSockJS();
+    }
 }
