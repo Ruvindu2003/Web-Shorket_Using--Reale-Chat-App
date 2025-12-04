3da1671876b86f387b890c981317c491da35480c
Ruvindu <ruvindusharada22@gmail.com>
2025-12-04 13:55:12 +0530

    intilize projects

--- src/main/java/com/example/demo/config/WebShorketConfig.java
+++ src/main/java/com/example/demo/config/WebShorketConfig.java
@@ -1,8 +1,13 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+
+@Configuration
+
 public class WebShorketConfig implements WebSocketMessageBrokerConfigurer {
+
 
 
 }
