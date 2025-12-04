389c73eb156a4c43e9837d8f12f6f1dadff07c95
Ruvindu <ruvindusharada22@gmail.com>
2025-12-04 13:58:31 +0530

    Initial commit

--- src/main/java/com/example/demo/config/WebShorketConfig.java
+++ src/main/java/com/example/demo/config/WebShorketConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 
 public class WebShorketConfig implements WebSocketMessageBrokerConfigurer {
+    
 
 
 
