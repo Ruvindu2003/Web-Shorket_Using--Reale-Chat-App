edf9a8a0c4ed8ccad696c3f1fb8762c04384bf34
Ruvindu <ruvindusharada22@gmail.com>
2025-12-04 13:56:10 +0530

    Add the WebShorketConfig.java

--- src/main/java/com/example/demo/config/WebShorketConfig.java
+++ src/main/java/com/example/demo/config/WebShorketConfig.java
@@ -1,10 +1,12 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 
 @Configuration
+@EnableWebSocketMessageBroker
 
 public class WebShorketConfig implements WebSocketMessageBrokerConfigurer {
 
