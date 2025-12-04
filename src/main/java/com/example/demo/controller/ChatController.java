f8558677632a7e8781c90e1ed1431b1072ed6cd7
Ruvindu <ruvindusharada22@gmail.com>
2025-12-04 14:31:50 +0530

    create the ChatController.java

---
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.ChatMassage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+@MessageMapping("/snedMassage")
+@SendTo("/topic/message")
+
+
+    //apSendMessage
+    public ChatMassage sendMassage(ChatMassage chatMassage){
+
+        return chatMassage;
+    }
+
+
+}
